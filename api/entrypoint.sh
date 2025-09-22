efc60af628b570b50b2ac47a7171311361463a2f
Dmytro Tymoshyk <dmytro.tymoshyk@optimalitypro.com>
2025-09-22 18:08:00 +0300

    added docker-compose

---
+++ api/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+echo "🚀 Running Prisma migrations..."
+npx prisma migrate deploy
+
+echo "✅ Starting API..."
+exec node dist/main.js
---
+++ api/prisma/migrations/20250921204915_add_ranout_status/migration.sql
@@ -0,0 +1,2 @@
+-- AlterEnum
+ALTER TYPE "GroceryItemStatus" ADD VALUE 'WANT';
