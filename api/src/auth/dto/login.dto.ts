dfe224324c7032ec0923cdc38df7b7a0d3f84e8f
Dmytro Tymoshyk <dmytro.tymoshyk@optimalitypro.com>
2025-09-23 01:14:48 +0300

    minor update

--- api/src/auth/dto/login.dto.ts
+++ api/src/auth/dto/login.dto.ts
@@ -2,11 +2,11 @@ import { ApiProperty } from '@nestjs/swagger'
 import { IsEmail, IsString } from 'class-validator'
 
 export class LoginDto {
-  @ApiProperty({ example: 'user@mail.com' })
+  @ApiProperty({ example: 'john@example.com' })
   @IsEmail()
   email: string
 
-  @ApiProperty({ example: 'secret123' })
+  @ApiProperty({ example: 'password123' })
   @IsString()
   password: string
 }
